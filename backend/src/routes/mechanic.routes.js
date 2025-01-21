3f9862d26c6d8a35e826bce120b143a988009c04
Denisexper <clasic.exper2023@gmail.com>
2025-01-20 23:06:51 -0600

    feat: enable token

--- backend/src/routes/mechanic.routes.js
+++ backend/src/routes/mechanic.routes.js
@@ -7,7 +7,7 @@ const app = express.Router()
 //creamos nueva instancia de la clase
 const mechanic = new MecanicoController()
 
-app.post("/create-mech", mechanic.createMechanic);
+app.post("/create-mech", verifyToken, mechanic.createMechanic);
 app.get("/get-mechs", mechanic.getMechanics);
 app.get("/get-mech/:id", mechanic.getMechanic);
 app.put("/update-mech/:id", mechanic.updateMechanic);
