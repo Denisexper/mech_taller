05cdcc43c67179677067f3e2bcd8e271a13d3d59
Denisexper <clasic.exper2023@gmail.com>
2025-01-20 23:07:27 -0600

    feat: next endpoint

--- node_modules/backend/app.js
+++ node_modules/backend/app.js
@@ -8,9 +8,13 @@ const app = express();
 app.use(express.json())
 
 app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+}))
+/* app.use(cors({
     origin: 'http://localhost:3000',
     credentials: true
-}));
+})); */
 
 const PORT = process.env.PORT || 8080;
 
